1201b014c0479602b355427655aa73597204cbbc
yjzx121 <724417336@qq.com>
2022-12-20 10:54:18 +0800

    change location of settings

--- typeidea/typeidea/wsgi.py
+++ typeidea/typeidea/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typeidea.settings')
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'typeidea.settings')
+profile = os.environ.get('TYPEIDEA_PROFILE', 'develop')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "typeidea.settings.%s" % profile)
 
 application = get_wsgi_application()
