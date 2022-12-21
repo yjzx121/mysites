df1ce570850fdaf6402658a1b5c86528b5ce3cfc
yjzx121 <724417336@qq.com>
2022-12-21 20:06:35 +0800

    little change

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,11 +1,31 @@
-from django.contrib import admin
 from django.urls import reverse
 from django.utils.html import format_html
 from .models import Post, Category, Tag
+from .adminforms import PostAdminForm
+from django.contrib import admin
+from typeidea.custom_site import custom_site
+
+# import requests
+# from django.contrib.auth import get_permission_codename
+
+
+# PERMISSION_API = "http://permission.sso.com/has_perm?user={}&perm_code={}"
+
+
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
+
+
+class PostInline(admin.TabularInline):    # StackedInline 样式不同
+    fields = ('title', 'desc')
+    extra = 1                             # 控制额外多几个
+    model = Post
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    inlines = [PostInline, ]
+
     list_display = ('name', 'status', 'is_nav', 'created_time')
     fields = ('name', 'status', 'is_nav')
 
@@ -49,14 +69,48 @@ class CategoryOwnerFilter(admin.SimpleListFilter):
         return queryset
 
 
-@admin.register(Post)
+@admin.register(Post, site=custom_site)
 class PostAdmin(admin.ModelAdmin):
     list_display = [
         'title', 'category', 'status',
         'created_time', 'operator'
     ]
-    list_display_links = []
 
+    exclude = ('owner', )
+
+    # 展示顺序配置 fields
+    # fields = (
+    #     ('category', 'title'),
+    #     'desc',
+    #     'status',
+    #     'content',
+    #     'tag',
+    # )
+
+    # 另一个配置fieldsets，展示顺序配置
+    fieldsets = (
+        ('基础配置', {
+            'description': '基础配置描述',
+            'fields': (
+                ('title', 'category'),
+                'status',
+            ),
+        }),
+        ('内容', {
+            'fields': (
+                'desc',
+                'content',
+            ),
+        }),
+        ('额外信息', {
+            'classes': ('collapse', ),
+            'fields': ('tag', ),
+        })
+    )
+    # filter_horizontal = ('tag', )
+    filter_vertical = ('tag', )
+
+    list_display_links = []
     list_filter = [CategoryOwnerFilter]
     search_fields = ['title', 'category_name']
 
@@ -66,23 +120,21 @@ class PostAdmin(admin.ModelAdmin):
     # 修改界面
     save_on_top = True
 
-    fields = (
-        ('category', 'title'),
-        'desc',
-        'status',
-        'content',
-        'tag',
-    )
-
     def __str__(self):
         return self.name
 
     def operator(self, obj):
         return format_html(
             '<a href="{}">编辑</a>',
-            reverse('admin:blog_post_change', args=(obj.id,))
+            reverse('cus_admin:blog_post_change', args=(obj.id,))
         )
     operator.short_description = "操作"
+
+    class Media:
+        css = {
+            'all': ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css", ),
+        }
+        js = ("https://cdn.bootcss.com/bootstrap/4.0.0-beta.2/js/bootstrap.bundle.js", )
 
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
@@ -91,3 +143,17 @@ class PostAdmin(admin.ModelAdmin):
     def get_queryset(self, request):
         qs = super(PostAdmin, self).get_queryset(request)
         return qs.filter(owner=request.user)
+
+    # 添加权限sso(Single Sign-On, 单点登录)
+    # 虚假地址 http://permission.sso.com/has_perm?user={}&perm_code={}
+    # def has_add_permission(self, request):
+    #     opts = self.opts
+    #     codename = get_permission_codename('add', opts)
+    #     perm_code = "%s.%s" % (opts.app_label, codename)
+    #     resp = requests.get(PERMISSION_API.format(request.user.username, perm_code))
+    #     if resp.status_code == 200:
+    #         return True
+    #     else:
+    #         return False
+
+
