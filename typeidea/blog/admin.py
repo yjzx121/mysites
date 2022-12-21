adc3772fb5d59f7d9206358a6d7e11eeaeb93476
yjzx121 <724417336@qq.com>
2022-12-21 10:30:49 +0800

    Post adding

--- typeidea/blog/admin.py
+++ typeidea/blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
 from .models import Post, Category, Tag
 
 
@@ -7,9 +9,17 @@ class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'is_nav', 'created_time')
     fields = ('name', 'status', 'is_nav')
 
+    def __str__(self):
+        return self.name
+
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(CategoryAdmin, self).save_model(request, obj, form, change)
+
+    def post_count(self, obj):
+        return obj.post_set.count()
+
+    post_count.short_description = 'Article Number'
 
 
 @admin.register(Tag)
@@ -17,6 +27,48 @@ class TagAdmin(admin.ModelAdmin):
     list_display = ('name', 'status', 'created_time')
     field = ('name', 'status')
 
+    def __str__(self):
+        return self.name
+
     def save_model(self, request, obj, form, change):
         obj.owner = request.user
         return super(TagAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'title', 'category', 'status',
+        'created_time', 'operator'
+    ]
+    list_display_links = []
+
+    list_filter = ['category', ]
+    search_fields = ['title', 'category_name']
+
+    actions_on_top = True
+    actions_on_bottom = True
+
+    # Edit HTML
+    save_on_top = True
+
+    fields = (
+        ('category', 'title'),
+        'desc',
+        'status',
+        'content',
+        'tag',
+    )
+
+    def __str__(self):
+        return self.name
+
+    def operator(self, obj):
+        return format_html(
+            '<a href="{}">EDIT</a>',
+            reverse('admin:blog_post_change', args=(obj.id,))
+        )
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(PostAdmin, self).save_model(request, obj, form, change)
