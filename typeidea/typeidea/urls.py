ced6b6f89ad8f3ec2e94c330fa08f723efda57cc
yjzx121 <724417336@qq.com>
2022-12-22 11:33:49 +0800

    add views

--- typeidea/typeidea/urls.py
+++ typeidea/typeidea/urls.py
@@ -15,9 +15,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
 from .custom_site import custom_site
+
+from blog.views import post_list, post_detail
+from config.views import links
+
 
 urlpatterns = [
     path('super_admin/', admin.site.urls),
     path('admin/', custom_site.urls),
+    url(r'^$', post_list),
+    url(r'^category/(?P<category_id>\d+)/$', post_list),
+    url(r'^tag/(?P<tag_id>\d+)/$', post_list),
+    url(r'^post/(?P<post_id>\d+).html$', post_detail),
+    url(r'^links/$', links),
 ]
